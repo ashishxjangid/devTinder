49b382e58076f8f7eda045cf111e0c2620b6a0eb
ashishxjangid <aj350398@gmail.com>
2025-06-27 14:20:27 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -46,6 +46,39 @@ app.use(
     }
 );
 
+// app.get("/admin/getalldata", (req, res) => {
+//     //logic of checking if the request is authorized
+//     const token= "xyza";
+//     const isAdminAuthorized= token==="xyz";
+//     if(isAdminAuthorized){
+//         res.send("All data sent");
+//     } 
+//     else{
+//         res.status(401).send("Unauthorized request");
+//     }
+// });
+
+//Handle auth middleware for get, post..... requests
+app.use("/admin", (req, res, next) => {
+    console.log("Admin auth is getting cheked");
+    const token= "xyz";
+    const isAdminAuthorized= token==="xyz";
+    if(isAdminAuthorized){
+        next();
+    }
+    else{
+        res.status(401).send("Unauthorized request");
+    }
+})
+
+app.get("/admin/getAllData", (req, res) => {
+    res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+    res.send("deleted user");
+});
+
 app.listen(3000, () => {
     console.log("server is listening on port 3000");
 });
